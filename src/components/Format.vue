73fe9a9c74df8ef66ccaf4a1ef335ca888e0e15b
Alf <alf.g.jr@gmail.com>
2019-11-05 22:00:52 -0800

    Separate Editor.vue into multiple form components.

---
+++ src/components/Format.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label for="container">Container</label>
+    <b-form-select
+      class="u-full-width"
+      :value="value"
+      @input="update"
+    >
+      <option :value="null" disabled>-- Please select an option --</option>
+      <optgroup v-for="(o, i) in containers" :label="i" v-bind:key="i">
+        <option v-for="item in o" :key="item.id" :value="item.value">{{item.name}}</option>
+      </optgroup>
+    </b-form-select>
+   </div>
+</template>
+
+<script>
+import config from '@/config';
+
+const {
+  containers,
+} = config;
+
+export default {
+  name: 'Format',
+  props: ['value'],
+  data() {
+    return {
+      containers,
+    };
+  },
+  methods: {
+    update(val) {
+      this.$emit('input', val);
+    },
+  },
+};
+</script>
