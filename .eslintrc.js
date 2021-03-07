09cadbcb704847f560aa0fae81f052edce782628
Alf <alf.g.jr@gmail.com>
2021-03-06 23:21:23 -0800

    update eslintrc rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
 
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'info' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 
